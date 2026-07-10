642760569a8678395291ed69494378c4396e7ba7
Your Name <your_email@example.com>
2026-07-10 21:19:27 +0500

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+<>
+  <App />
+  <SpeedInsights />
+</>
